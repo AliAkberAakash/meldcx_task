8acdeea1826859a7857a22630e46ba8fa259ee84
aliakberaakash <ali852609@gmail.com>
2021-09-22 00:06:17 +0600

    added webView loading functionality

--- app/src/main/java/com/xlsoft/meldcxtask/ui/features/home/HomeFragment.kt
+++ app/src/main/java/com/xlsoft/meldcxtask/ui/features/home/HomeFragment.kt
@@ -1,9 +1,20 @@
 package com.xlsoft.meldcxtask.ui.features.home
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.xlsoft.meldcxtask.R
 import com.xlsoft.meldcxtask.core.ui.BaseFragment
 import com.xlsoft.meldcxtask.databinding.FragmentHomeBinding
+import com.xlsoft.meldcxtask.ui.utils.makeItGone
+import com.xlsoft.meldcxtask.ui.utils.makeItVisible
+import com.xlsoft.meldcxtask.ui.utils.showShortToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,5 +23,53 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private val _viewModel : HomeViewModel by viewModels()
 
     override fun getLayoutId() = R.layout.fragment_home
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupWebView()
+        setListeners()
+
+    }
+
+    /**
+     * Setup the [WebView] configurations
+     */
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun  setupWebView(){
+
+        // Enable JavaScript
+        val settings = binding.webView.settings
+        settings.javaScriptEnabled = true
+
+        // Setup WebViewClient to load url inside the app
+        // and not in default browser
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.loader.makeItGone()
+                super.onPageFinished(view, url)
+            }
+
+        }
+    }
+
+    private fun setListeners(){
+        binding.goButton.setOnClickListener {
+            val url = binding.urlField.text.toString().trim()
+            if(url.isEmpty()){
+                showShortToast(requireContext(), "Please enter a url")
+            }else{
+                binding.loader.makeItVisible()
+                binding.webView.loadUrl(url)
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/xlsoft/meldcxtask/ui/utils/ViewUtils.kt
@@ -0,0 +1,21 @@
+package com.xlsoft.meldcxtask.ui.utils
+
+import android.view.View
+
+fun View.isVisible(): Boolean = visibility == View.VISIBLE
+
+fun View.isGone(): Boolean = visibility == View.GONE
+
+fun View.isInvisible(): Boolean = visibility == View.INVISIBLE
+
+fun View.makeItVisible() {
+    visibility = View.VISIBLE
+}
+
+fun View.makeItInvisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.makeItGone() {
+    visibility = View.GONE
+}
