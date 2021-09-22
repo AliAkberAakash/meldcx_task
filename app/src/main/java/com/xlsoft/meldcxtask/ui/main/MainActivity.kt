085540156ba173def9b0b40d7be17c31d9ebedbe
aliakberaakash <ali852609@gmail.com>
2021-09-22 23:37:11 +0600

    improved navigation

--- app/src/main/java/com/xlsoft/meldcxtask/ui/main/MainActivity.kt
+++ app/src/main/java/com/xlsoft/meldcxtask/ui/main/MainActivity.kt
@@ -3,6 +3,10 @@ package com.xlsoft.meldcxtask.ui.main
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.xlsoft.meldcxtask.R
 import com.xlsoft.meldcxtask.core.ui.BaseActivity
 import com.xlsoft.meldcxtask.data.datasources.localdb.SearchHistoryDao
@@ -13,10 +17,26 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(){
 
-    private val _viewModel : MainViewModel by  viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            navController.graph,
+        )
+
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration
+        )
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun getLayoutId() = R.layout.activity_main
